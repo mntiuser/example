4f702c1cc8b80de1fa3ff52e14a6a52d95274da3
mntiuser <mnti4694@gmail.com>
2024-09-03 19:54:00 +0530

    hi

---
+++ thy.py
@@ -0,0 +1,9 @@
+print("yrdgikg")
+import matplotlib.pyplot as plt
+x=[2,3,3,6,2,5,6,3,]
+y=[2,5,6,4,3,2,6,3,]
+plt.scatter(x,y,label='value of x and y',color='red')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('scatterplot')
+plt.show()
